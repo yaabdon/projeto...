f00c6f55e370ec90cc25a40db940d0a67e6511b7
yaabdon <yasminabdon1@gmail.com>
2025-01-07 13:49:41 -0300

    meu terceiro commit

--- assets/sounds/audio.py
+++ assets/sounds/audio.py
@@ -1,46 +1,30 @@
 import pygame
-import os
 
-# Configurar backend de áudio para "dummy" no caso de ambientes sem áudio
-os.environ["SDL_AUDIODRIVER"] = "dummy"
-
-try:
-    pygame.init()
-    pygame.mixer.init()
-    audio_disponivel = True
-except pygame.error as e:
-    print(f"Áudio desativado: {e}")
-    audio_disponivel = False
+pygame.init()
+pygame.mixer.init()
+audio_disponivel = True
 
 class Audio:
     @staticmethod
     def tocar_som(caminho_arquivo):
         if audio_disponivel:
-            try:
-                som = pygame.mixer.Sound(caminho_arquivo)
-                som.play()
-            except pygame.error as e:
-                print(f"Erro ao tocar som: {e}")
+            som = pygame.mixer.Sound(caminho_arquivo)
+            som.play()
         else:
             print("Áudio não disponível para tocar som.")
 
     @staticmethod
     def tocar_musica(caminho_arquivo):
         if audio_disponivel:
-            try:
-                pygame.mixer.music.load(caminho_arquivo)
-                pygame.mixer.music.play(-1)  # -1 para loop infinito
-            except pygame.error as e:
-                print(f"Erro ao tocar música: {e}")
+            pygame.mixer.music.load(caminho_arquivo)
+            pygame.mixer.music.play(-1)  # -1 para loop infinito
         else:
             print("Áudio não disponível para tocar música.")
 
     @staticmethod
     def parar_musica():
         if audio_disponivel:
-            try:
-                pygame.mixer.music.stop()
-            except pygame.error as e:
-                print(f"Erro ao parar música: {e}")
+            pygame.mixer.music.stop()
         else:
             print("Áudio não disponível para parar música.")
+
