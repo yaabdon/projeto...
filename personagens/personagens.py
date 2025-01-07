82f1ce9d4cfb56db61e5f2ca1c47f5a29feda636
yaabdon <yasminabdon1@gmail.com>
2025-01-07 01:27:47 -0300

    Add files via upload

---
+++ personagens/personagens.py
@@ -0,0 +1,17 @@
+import json
+
+class Personagem:
+    @staticmethod
+    def eleanor():
+        return {
+            "nome": "Eleanor",
+            "descrição": "Loira, olhos violeta vivos. Não leva desaforo para casa",
+            "idade": 17
+        }
+    @staticmethod
+    def vitoria():
+        return {
+            "nome": "Vitor",
+            "descrição": "Cabelos negros, olhos mel. Atraente e inescrupuloso",
+            "idade": 19
+        }
