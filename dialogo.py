833c4a2a60972bef2154a08eece9d1bfba99d4bc
yaabdon <yasminabdon1@gmail.com>
2025-01-07 01:30:52 -0300

    Delete dialogo.py

--- dialogo.py
+++
@@ -1,8 +0,0 @@
-class Dialogo:
-    def __init__(self, personagem, mensagem, condicao=None):
-        self.personagem = personagem
-        self.mensagem = mensagem
-        self.condicao = condicao
-
-    def str(self):
-        return f"{self.personagem}: {self.mensagem}"
